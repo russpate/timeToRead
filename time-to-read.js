684a7b249c2be8baa6fc8e57a2ab12719ac6f86f
Russell Pate <russ@russpate.com>
2016-02-16 12:39:12 -0500

    final push before submitting

--- time-to-read.js
+++ time-to-read.js
@@ -1,15 +1,17 @@
-var bodyContent = document.getElementsByTagName("p");
+
 
 var timeToRead = function() {
-  var allWords = "";
+  var bodyContent = document.getElementsByTagName("p");
   var contentArray = Array.prototype.slice.call(bodyContent);
+  var allWords = "";
 
   contentArray.forEach(function(item,idx,array){
     allWords += item.innerText;
   });
 
   var wordArray = allWords.split(" ");
-  return ("about " + Math.round(wordArray.length/230) + " minutes to read");
+  return ("about " + Math.round(wordArray.length/230) + " min read");
 };
 
 document.getElementsByClassName('reading-time')[0].innerText = timeToRead();
+// var displayTime = document.createElement("h3");[0].innerText = timeToRead();
